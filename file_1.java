d535b8c716361dd941c4f605e803e77c26d0b1de
shreekar <shreekar157200419@gmail.com>
2025-01-01 12:00:00 +0530

    Add file_1.java

---
+++ file_1.java
@@ -0,0 +1,48 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int val){
+        this.data = val;
+        this.next = null;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+      Node first = new Node(10);
+      Node second  = new Node(20);
+      Node third = new Node(30);
+      Node four = new Node(40);
+      
+      first.next = second;
+      second.next = third;
+      third.next = four;
+      
+      Node head , temp;
+      head = first ;
+      temp = head;
+      
+      // delete node at end;
+      for(;temp.next.next != null;){
+          temp = temp.next;
+      }
+      
+      temp.next = null;
+      
+      //print the linked list
+      temp = head;
+      
+      for(;temp!=null;){
+          System.out.print(" " + temp.data);
+          temp = temp.next;
+      }
+      
+      
+
+
+    }
+}
